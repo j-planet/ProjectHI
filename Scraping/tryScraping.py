757d057fdc026db8c5a2fc1c031af7e993e385aa
jennyyuejin <jin.yuejenny@gmail.com>
2015-01-03 21:15:02 -0800

    added some url utilities. scraped some  bloomingdale pics.

--- Scraping/tryScraping.py
+++ Scraping/tryScraping.py
@@ -43,17 +43,59 @@ def get_page_image_urls(pageLink):
             yield r
 
 
-def download_pic(link, outputDir):
+def is_full_url(url):
+    """
+    './blah' -> false; 'http://google.com' -> true
+    :return: Boolean
+    """
+
+    return urlparse.urlparse(url).scheme != ''
+
+
+def extract_root_url(url):
+    """
+    http://google.com/blahblah -> http://google.com
+    :return: string. Raises exception if url doesn't contain a root url (e.g. './blahblah')
+    """
+
+    if not is_full_url(url):
+        raise ValueError('The given url (%s) contains no root url.' % url)
+
+    temp = urlparse.urlparse(url)
+    scheme = temp.scheme
+    netloc = temp.netloc
+
+    return scheme + '://' + netloc
+
+
+def make_full_url(link, urlWithScheme = None):
+    """
+    returns the original url if it's already full
+    :return: string
+    """
+
+    if is_full_url(link):
+        return link
+    else:
+        return urlparse.urljoin(extract_root_url(urlWithScheme), link)
+
+
+def download_pic(link, outputDir, suffix=None, homeUrl=None):
     """
     downloads a single pic
+    :param suffix: extra string to insert at the end of the file name
+    :param homeUrl: an URL containing the root/base url. not needed if the link is already a full url
     :return: output file path
     """
 
     # get pic object
-    rawPic = requests.get(link, stream=True).raw
+    rawPic = requests.get(make_full_url(link, homeUrl), stream=True).raw
 
     # write to file
-    outputFpath = os.path.join(outputDir, os.path.basename(urlparse.urlparse(link).path))
+    name, ext = os.path.splitext(os.path.basename(urlparse.urlparse(link).path))
+    filename = name + ('' if suffix is None else '_' + str(suffix)) + '.' + ext
+
+    outputFpath = os.path.join(outputDir, filename)
 
     with open(outputFpath, 'wb') as f:
         shutil.copyfileobj(rawPic, f)
@@ -62,15 +104,12 @@ def download_pic(link, outputDir):
     del rawPic
 
 
+pageURL = 'http://bloomingdalesfashionpacked.com/closet-what-to-wear-where.aspx?cm_sp=NAVIGATION-_-TOP_NAV-_-2910-CATEGORY_ICON_IMAGE-January---What-to-Wear-Where#closet'
 imgOutputDir = '/Users/JennyYueJin/ProjectHI/Scraping/output'
 
-list(get_page_image_urls('https://plus.google.com/photos/+HawaiianPaddleSports/albums/6097270274560596833'))
+# pprint(list(get_page_image_urls(pageURL)))
 
-pprint(list(get_page_image_urls('https://www.facebook.com/')))
-
-download_pic('https://lh6.googleusercontent.com/_FEVkydLItTgYfvxmyQXPKG0B1Fo2yq5br3wRg2Mc5Nd=w1477-h1108-no', imgOutputDir)
-
-for picLink in get_page_image_urls('https://www.facebook.com/'):
+for counter, picLink in enumerate(get_page_image_urls(pageURL)):
     print picLink
-    download_pic(picLink, imgOutputDir)
+    download_pic(picLink, imgOutputDir, suffix=counter, homeUrl=pageURL)
 
